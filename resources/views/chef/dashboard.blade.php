bdf2befa7da04d68063a7c439b32b8a5c29e3613
IMANE EL AIBOUD <imaneelaiboud@gmail.com>
2022-01-12 22:49:42 +0100

    Realiser le dashboard pour different utilisateurs

--- resources/views/chef/dashboard.blade.php
+++ resources/views/chef/dashboard.blade.php
@@ -4,89 +4,38 @@
 <li class="nav-item">
     <a class="nav-link" href="index.html">
         <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
+        <span>Tableau de bord</span></a>
 </li>
 
 <!-- Divider -->
 <hr class="sidebar-divider">
 
-<!-- Heading -->
-<div class="sidebar-heading">
-    Interface
-</div>
-
 <!-- Nav Item - Pages Collapse Menu -->
+<hr class="sidebar-divider">
 <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-        aria-expanded="true" aria-controls="collapseTwo">
-        <i class="fas fa-fw fa-cog"></i>
-        <span>Utilisateurs</span>
-    </a>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Les utilisateurs:</h6>
-            <a class="collapse-item" href="#">Chefs de projet</a>
-            <a class="collapse-item" href="#">Responsables d'achat</a>
-            <a class="collapse-item" href="#">Entreprises</a>
-        </div>
-    </div>
+    <a class="nav-link" href="charts.html">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Mon compte</span></a>
 </li>
 
-<!-- Nav Item - Utilities Collapse Menu -->
+<!-- Nav Item - Pages Collapse Menu -->
 <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw fa-wrench"></i>
-        <span>Gestion</span>
+        <span>Mes Projets</span>
     </a>
     <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Gestion:</h6>
-            <a class="collapse-item" href="utilities-color.html">Roles</a>
-            <a class="collapse-item" href="utilities-border.html">Permission</a>
+            <h6 class="collapse-header">Mes projets:</h6>
+            <a class="collapse-item" href="utilities-color.html">Créer un nouveau projet</a>
+            <a class="collapse-item" href="utilities-color.html">En cours</a>
+            <a class="collapse-item" href="utilities-border.html">Terminer</a>
+            <a class="collapse-item" href="utilities-border.html">Tous les projets</a>
         </div>
     </div>
 </li>
-
-<!-- Divider -->
-<hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-    Addons
-</div>
-
-<!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item active">
-    <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-        aria-controls="collapsePages">
-        <i class="fas fa-fw fa-folder"></i>
-        <span>Projets</span>
-    </a>
-    <div id="collapsePages" class="collapse show" aria-labelledby="headingPages"
-        data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Les Projets:</h6>
-            <a class="collapse-item" href="#">En cours</a>
-            <a class="collapse-item" href="#">Terminer</a>
-            <a class="collapse-item" href="#">Annuler</a>
-            <a class="collapse-item" href="#">Tous les projets</a>
-            {{-- <div class="collapse-divider"></div>
-            <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item" href="404.html">404 Page</a>
-            <a class="collapse-item active" href="blank.html">Blank Page</a> --}}
-        </div>
-    </div>
-</li>
-
-<!-- Nav Item - Charts -->
-<li class="nav-item">
-    <a class="nav-link" href="charts.html">
-        <i class="fas fa-fw fa-chart-area"></i>
-        <span>Statistiques</span></a>
-</li>
-
 
 @endsection
 
